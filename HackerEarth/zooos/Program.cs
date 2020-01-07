64982aa5ab061bda42ed9e91c1728421d736b2e8
chinmay6345 <chinmay8116@gmail.com>
2020-01-07 22:06:57 +0530

    commit-zoos

---
+++ HackerEarth/zooos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace zooos
+{
+    public static class AppHelper
+    {
+        public static String IsIndentical(String input)
+        {
+            return ((input.Where(a => a == 'z').Count() * 2) == (input.Length - input.Where(a => a == 'z').Count() )) ? "YES" : "NO";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            Console.WriteLine(AppHelper.IsIndentical(input));
+            Console.ReadLine();
+        }
+    }
+}
