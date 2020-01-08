d1c1745ac04c482092fb88c70422dd142dd82808
chinmay6345 <chinmay8116@gmail.com>
2020-01-08 08:30:09 +0530

    commit-minmax

---
+++ HackerEarth/MinMax/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinMax
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32 n = Int32.Parse(Console.ReadLine());
+            List<int> lst=Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(a => int.Parse(a)).ToList();
+            int minsum = lst.Sum() - lst.Max();
+            int maxsum = lst.Sum() - lst.Min();
+            Console.WriteLine(minsum + " " + maxsum);
+            Console.ReadLine();
+        }
+
+    }
+}
