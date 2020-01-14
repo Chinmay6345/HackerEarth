7ec06013daa2e130c5cd607d6b18d725cca81b4a
chinmay6345 <chinmay8116@gmail.com>
2020-01-14 09:02:14 +0530

    commit-mobile no

---
+++ HackerEarth/What is your mobile number/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace What_is_your_mobile_number
+{
+    public static class AppHelper
+    {
+        public static List<String> lstValidNumbers(List<String> lstinput)
+        {
+            List<String> lstOutput = new List<String>();
+            foreach (String input in lstinput)
+            {
+                if (input.All(x => !Char.IsLetter(x)))
+                {
+                    if (input.StartsWith("0") || input.Length < 10 || input.Contains("."))
+                    {
+                        lstOutput.Add("NO");
+                    }
+                    else
+                    {
+                        lstOutput.Add("YES");
+                    }
+                }
+                else
+                {
+                    lstOutput.Add("NO");
+                }
+            }
+            return lstOutput;
+        }
+    }
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int noOfCases = int.Parse(Console.ReadLine());
+            List<String> ListMobileNo = new List<String>();
+            while (noOfCases != 0)
+            {
+                String mobilenumber = Convert.ToString(Console.ReadLine());
+                ListMobileNo.Add(mobilenumber);
+                noOfCases--;
+            }
+            List<String> lst = AppHelper.lstValidNumbers(ListMobileNo);
+            ListMobileNo = new List<String>();
+            lst.ForEach(k => { Console.WriteLine(k); });
+            lst = new List<String>();
+        }
+    }
+}
