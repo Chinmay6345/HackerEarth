bd14b197cd7765e497a87d151d729e517b5143b1
chinmay6345 <chinmay8116@gmail.com>
2020-01-15 10:09:06 +0530

    commit

---
+++ HackerEarth/Simple Search/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Search
+{
+    //https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/simple-search-4/
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32 ArraySize = Convert.ToInt32(Console.ReadLine());
+            Int32[] array = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(element => Convert.ToInt32(element)).ToArray<Int32>();
+            Int32 elementToSearch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Array.IndexOf(array, elementToSearch));
+        }
+    }
+}
