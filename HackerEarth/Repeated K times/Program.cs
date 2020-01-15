911334c6cf35e0382db8a52f4a01ea886896a0c1
chinmay6345 <chinmay8116@gmail.com>
2020-01-15 10:11:12 +0530

    commit

---
+++ HackerEarth/Repeated K times/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeated_K_times
+{
+    //https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/repeated-k-times/
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32 arraySize = Int32.Parse(Console.ReadLine());
+            Int32[] array = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(element => Convert.ToInt32(element)).ToArray<Int32>();
+            Int32 k = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(array.GroupBy(a => a).Where(a => a.Count() == k).Select(a => Convert.ToInt32(a.Key.ToString())).Min());
+        }
+    }
+}
