f5310fd361cfc2ca9a68e295a7b64a611b6e590f
chinmay6345 <chinmay8116@gmail.com>
2020-01-16 07:06:13 +0530

    commit

---
+++ HackerEarth/Alphanumeric Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+//https://www.hackerearth.com/problem/algorithm/alphanumeric-strings/
+
+namespace Alphanumeric_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String InputString = Console.ReadLine();
+            Console.WriteLine(InputString.Where(a=>Char.IsNumber(a)).Select(a => a - '0').ToArray().Sum());
+        }
+    }
+}
