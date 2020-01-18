e257000727c944022024913d1bf15e8918d98e06
chinmay6345 <chinmay8116@gmail.com>
2020-01-18 16:51:55 +0530

    commit

---
+++ HackerEarth/Array Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Sum
+{
+    //https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/array-sum-2-725368ac/
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Int32 n = Int32.Parse(Console.ReadLine());
+            Int64[] array = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                           .Select(a => Int64.Parse(a)).ToArray<Int64>();
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
