7cc272318895ca40b33b0df0ac4fcad7726c3a7c
chinmay6345 <chinmay8116@gmail.com>
2020-01-19 08:53:39 +0530

    commit

--- HackerEarth/Array Sum/Program.cs
+++ HackerEarth/Array Sum/Program.cs
@@ -11,10 +11,14 @@ namespace Array_Sum
     {
         public static void Main(String[] args)
         {
-            Int32 n = Int32.Parse(Console.ReadLine());
-            Int64[] array = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                                           .Select(a => Int64.Parse(a)).ToArray<Int64>();
-            Console.WriteLine(array.Sum());
+            //Int32 n = Int32.Parse(Console.ReadLine());
+            //Int64[] array = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            //                               .Select(a => Int64.Parse(a)).ToArray<Int64>();
+            //Console.WriteLine(array.Sum());
+
+            int [] array = {2,2,1,1,1,2,2 };
+            int p = array.GroupBy(a => a).Select(a => new { Key = a.Key, count = a.Count() }).Max(a => a.count);
+            Console.ReadLine();
         }
     }
 }
---
+++ HackerEarth/Rajan and odd frequency number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rajan_and_odd_frequency_number
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Int32 n = Convert.ToInt32(Console.ReadLine());
+            Int32[] array = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(a => Convert.ToInt32(a)).ToArray<Int32>();
+            Int32 result = array.GroupBy(a => a).Where(a => a.Count() == 1).Select(a => Convert.ToInt32(a.Key.ToString())).First();
+            Console.WriteLine(result);
+        }
+    }
+}
