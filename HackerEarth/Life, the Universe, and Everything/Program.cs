1ef3536382c3745ecc88b6d4b73644d1e216d12b
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 16:47:26 +0530

    commit

---
+++ HackerEarth/Life, the Universe, and Everything/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/
+
+namespace Life__the_Universe__and_Everything
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                Int32 k = Convert.ToInt32(Console.ReadLine());
+                if (k.Equals(42))
+                {
+                    return;
+                }
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
