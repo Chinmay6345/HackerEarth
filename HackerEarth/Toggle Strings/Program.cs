d62b1327b90114f2a7d6af9df8c375a1c95f0927
chinmay6345 <chinmay8116@gmail.com>
2020-02-16 08:45:56 +0530

    commit

---
+++ HackerEarth/Toggle Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/modify-the-string/
+
+namespace Toggle_Strings
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            String input = Console.ReadLine();
+            StringBuilder builder = new StringBuilder(input);
+            for (int i = 0; i < builder.Length; i++)
+            {
+                if (Char.IsLower(builder[i]))
+                {
+                    builder[i] = Char.ToUpper(builder[i]);
+                }
+                else
+                {
+                    builder[i] = Char.ToLower(builder[i]);
+                }
+            }
+            Console.WriteLine(Convert.ToString(builder));
+        }
+    }
+}
