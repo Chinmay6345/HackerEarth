01399eae506fdc773ba42ebee733c6f74bd36e78
chinmay6345 <chinmay8116@gmail.com>
2020-02-21 19:54:47 +0530

    commit

---
+++ HackerEarth/Palindrome String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/palindrome-check-2/
+
+namespace Palindrome_String
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            String A = Console.ReadLine();
+            Char[] array = A.ToCharArray();
+            Array.Reverse(array);
+            String newString = new String(array);
+            if (A.Equals(newString))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
