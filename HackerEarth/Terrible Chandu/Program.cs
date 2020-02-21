47fdd965132910900df3621ba0b7f418f6f2a2b1
chinmay6345 <chinmay8116@gmail.com>
2020-02-21 19:11:12 +0530

    commit

---
+++ HackerEarth/Terrible Chandu/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//https://www.hackerearth.com/practice/algorithms/string-algorithm/basics-of-string-manipulation/practice-problems/algorithm/terrible-chandu/description/
+
+namespace Terrible_Chandu
+{
+    public static class AppHelper
+    {
+        public static void ReverseString(String str)
+        {
+            Char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            Console.WriteLine(new String(arr));
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Int32 NoOfTestCases = Int32.Parse(Console.ReadLine());
+            while (NoOfTestCases != 0)
+            {
+                String input = Console.ReadLine();
+                AppHelper.ReverseString(input);
+                NoOfTestCases = NoOfTestCases - 1;
+            }
+        }
+    }
+}
