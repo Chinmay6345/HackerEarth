e3e00c52ded569f33e81f327ec84ffb08bbb50fe
chinmay6345 <chinmay8116@gmail.com>
2020-02-21 20:25:41 +0530

    commit

---
+++ HackerEarth/Fun with Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://www.hackerearth.com/problem/algorithm/fun-with-strings/description/
+
+namespace Fun_with_Strings
+{
+    public class Program
+    {
+        public static long getcount(long n)
+        {
+            long[] array = new long[n + 1];
+            array[1] = 2;
+            array[2] = 3;
+            for (int i = 3; i <= n; i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+            return array[n];
+        }
+
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(getcount(n));
+
+        }
+    }
+}
