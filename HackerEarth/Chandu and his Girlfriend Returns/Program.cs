fb9d39d01124e3d5ea63e1f42546eefe7f07962d
chinmay6345 <chinmay8116@gmail.com>
2020-03-15 09:13:10 +0530

    commit

---
+++ HackerEarth/Chandu and his Girlfriend Returns/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+In the previous problem Chandu bought some unsorted arrays and sorted them (in non-increasing order). Now, he has many sorted arrays to give to his girlfriend. But, the number of sorted arrays are very large so Chandu decided to merge two sorted arrays into one sorted array. But he is too lazy to do that. So, he asked your help to merge the two sorted arrays into one sorted array (in non-increasing order).
+
+Input:
+First line contains an integer T, denoting the number of test cases.
+First line of each test case contains two space separated integers N and M, denoting the size of the two sorted arrays.
+Second line of each test case contains N space separated integers, denoting the first sorted array A.
+Third line of each test case contains M space separated integers, denoting the second array B.
+
+Output:
+For each test case, print (N + M) space separated integer representing the merged array.
+
+Constraints:
+1 <= T <= 100
+1 <= N, M <= 5*104
+0 <= Ai, Bi <= 109
+
+SAMPLE INPUT 
+1
+4 5
+9 7 5 3
+8 6 4 2 0
+SAMPLE OUTPUT 
+9 8 7 6 5 4 3 2 0
+ 
+*/
+//https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/practice-problems/algorithm/chandu-and-his-girlfriend-returns/
+namespace Chandu_and_his_Girlfriend_Returns
+{
+    public static class AppHelper
+    {
+        public static void Merge(Int64[] A, Int64[] B)
+        {
+            Int64[] C = A.Concat(B).OrderByDescending(g => g).ToArray<Int64>();
+            Console.WriteLine(String.Join(" ", C.Select(g => g)));
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Int32 n = Int32.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                Int64[] arr = Console.ReadLine()
+                              .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(a => Int64.Parse(a))
+                            .ToArray<Int64>();
+                Int64[] A = Console.ReadLine()
+                              .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(a => Int64.Parse(a))
+                            .ToArray<Int64>();
+                Int64[] B = Console.ReadLine()
+                              .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(a => Int64.Parse(a))
+                            .ToArray<Int64>();
+                AppHelper.Merge(A, B);
+                n = n - 1;
+            }
+        }
+    }
+}
---
+++ HackerEarth/Chandu and his Girlfriend Returns/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Chandu and his Girlfriend Returns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chandu and his Girlfriend Returns")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bca9f946-7638-45af-b8ea-38cc20cc34fb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
