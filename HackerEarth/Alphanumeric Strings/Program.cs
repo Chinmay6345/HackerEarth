7103c75c49f1ef3aa329419bad371df730c7c394
chinmay6345 <chinmay8116@gmail.com>
2020-03-20 19:32:47 +0530

    added description for alphanumeric strings

--- HackerEarth/Alphanumeric Strings/Program.cs
+++ HackerEarth/Alphanumeric Strings/Program.cs
@@ -3,6 +3,10 @@ using System.Linq;
 
 //https://www.hackerearth.com/problem/algorithm/alphanumeric-strings/
 
+/*You are given a string which consists of lowercase, uppercase alphabets and numbers in it. Such strings are called alphanumeric strings.
+
+Your task is simple, you just need to separate the numbers from the alphanumeric string and output their sum.*/
+
 namespace Alphanumeric_Strings
 {
     class Program
