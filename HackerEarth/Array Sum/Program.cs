abaa1d127634e3dfecae557a9dae00b80ff96b7c
chinmay6345 <chinmay8116@gmail.com>
2020-03-21 22:19:10 +0530

    commit

--- HackerEarth/Array Sum/Program.cs
+++ HackerEarth/Array Sum/Program.cs
@@ -11,14 +11,10 @@ namespace Array_Sum
     {
         public static void Main(String[] args)
         {
-            //Int32 n = Int32.Parse(Console.ReadLine());
-            //Int64[] array = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-            //                               .Select(a => Int64.Parse(a)).ToArray<Int64>();
-            //Console.WriteLine(array.Sum());
-
-            int [] array = {2,2,1,1,1,2,2 };
-            int p = array.GroupBy(a => a).Select(a => new { Key = a.Key, count = a.Count() }).Max(a => a.count);
-            Console.ReadLine();
+            Int32 n = Int32.Parse(Console.ReadLine());
+            Int64[] array = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                           .Select(a => Int64.Parse(a)).ToArray<Int64>();
+            Console.WriteLine(array.Sum());
         }
     }
 }
