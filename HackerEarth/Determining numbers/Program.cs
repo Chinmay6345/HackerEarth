18bb76f4a238ef64837017680590148cab51519c
chinmay6345 <chinmay8116@gmail.com>
2020-03-22 20:56:46 +0530

    added description

--- HackerEarth/Determining numbers/Program.cs
+++ HackerEarth/Determining numbers/Program.cs
@@ -5,7 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 //https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/find-the-numbers-75f24949/
+/*
+You are given an array of integers. The special property of the array is that exactly two different elements occur once while other elements occur twice.
 
+You are required to determine those two elements.
+
+Input format
+
+First line: Integer  denoting the number of elements in the array
+Second line:  space-separated integers denoting the values in the array
+Output format
+
+Print two space-separated integers that occur once in the array in ascending order.
+
+Constraints
+
+
+, where  denotes the  element in the array
+
+SAMPLE INPUT 
+8
+1 2 3 4 5 5 3 4
+SAMPLE OUTPUT 
+1 2 
+     
+*/
 namespace Determining_numbers
 {
     public class Program
