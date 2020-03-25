2dfcfb725c12ef2479f72801ae5cf85a2a064722
chinmay6345 <chinmay8116@gmail.com>
2020-03-25 21:58:32 +0530

    added description-special shop

--- HackerEarth/Special shop/Program.cs
+++ HackerEarth/Special shop/Program.cs
@@ -1,7 +1,42 @@
 ﻿using System;
 using System.Linq;
 //https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/special-shop-69904c91/
+/*
+Creatnx now wants to decorate his house by flower pots. He plans to buy exactly  ones. He can only buy them from Triracle's shop. There are only two kind of flower pots available in that shop. The shop is very strange. If you buy  flower pots of kind 1 then you must pay  and  if you buy  flower pots of kind 2. Please help Creatnx buys exactly  flower pots that minimizes money he pays.
 
+Input Format
+
+The first line contains a integer  denoting the number of test cases.
+
+Each of test case is described in a single line containing three space-separated integers .
+
+Output Format
+
+For each test case, print a single line containing the answer.
+
+Constraints
+
+ 
+
+SAMPLE INPUT 
+2
+5 1 2
+10 2 4
+
+SAMPLE OUTPUT 
+17
+134
+Explanation
+Query 1: we have to buy exactly  pots. There are six possible options:
+
+Buy  pot of first kind,  pots of second kind. The cost is: .
+Buy  pot of first kind,  pots of second kind. The cost is: .
+Buy  pots of first kind,  pots of second kind. The cost is: .
+Buy  pots of first kind,  pots of second kind. The cost is: .
+Buy  pots of first kind,  pot of second kind. The cost is: .
+Buy  pots of first kind,  pot of second kind. The cost is: .
+So, the optimal cost is .
+*/
 namespace Special_shop
 {
     public static class AppHelper
