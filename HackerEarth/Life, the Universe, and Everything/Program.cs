4185cfd099c45b3305108e15eb441e90d69ae41e
chinmay6345 <chinmay8116@gmail.com>
2020-03-26 19:21:27 +0530

    added description

--- HackerEarth/Life, the Universe, and Everything/Program.cs
+++ HackerEarth/Life, the Universe, and Everything/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
 //https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/
-
+/*
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits. 
+*/
 namespace Life__the_Universe__and_Everything
 {
     public class Program
