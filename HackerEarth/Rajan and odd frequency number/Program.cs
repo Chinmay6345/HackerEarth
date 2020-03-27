1a2c4d18ed0f904fdcd79ec442b84169e1367b3d
chinmay6345 <chinmay8116@gmail.com>
2020-03-27 20:12:54 +0530

    commit-description

--- HackerEarth/Rajan and odd frequency number/Program.cs
+++ HackerEarth/Rajan and odd frequency number/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+//https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/danny-and-his-loneliness/
+/*
+Given an array of numbers of size (2*n+1).Raja is unable to find the number which is present odd number of times.It is guaranteed that only one such number exists.Can you help Raja in finding the number which is present odd number of times?
+
+Input
+First line contains value of n.
+Second line contains (2*n+1) array elements.
+Output
+Print the required number.
+Constraints
+1≤ n ≤ 
+1≤ a[i] ≤ 
+
+SAMPLE INPUT 
+2
+1 2 3 2 1
+SAMPLE OUTPUT 
+3 
+*/
 namespace Rajan_and_odd_frequency_number
 {
     public class Program
